14feede7528d12fa5a1143cf21c47b08c99305b4
vaneguz <vaneguz27@gmail.com>
2016-11-18 11:46:55 -0500

    LISTO SOLO FRONT: index y crear

--- app/router.js
+++ app/router.js
@@ -9,6 +9,10 @@ const Router = Ember.Router.extend({
 Router.map(function() {
   this.route('about');
   this.route('contact');
+  this.route('receta');
+  this.route('listarReceta');
+  this.route('crearReceta');
+  this.route('detalleReceta');
 });
 
 export default Router;
